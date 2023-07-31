ed1b80ea345c58b5716e68f38868668bae6108e6
therealnv6 <devrawr@protonmail.com>
2023-07-31 19:20:08 +0200

    fix(themes): recursively import themes in node server through postcss to circumvent client-sided CSP

--- src/theme.ts
+++ src/theme.ts
@@ -1,20 +1,69 @@
 import { Window } from "@gluon-framework/gluon";
 import { log, LogLevel, settings } from "./util.ts";
+import postcss, { ProcessOptions } from "postcss/lib/postcss";
 
 /**
  * Fetches the theme configuration from an external URL.
  * @returns {Promise<string>} The CSS style content.
  */
 async function fetchTheme(): Promise<string> {
-  const response = await fetch(settings.themes);
+  return processCSS(settings.themes);
+}
+
+/**
+ * Process the CSS content using PostCSS with necessary plugins.
+ * @param {string} url - The URL of the CSS file.
+ * @returns {Promise<string>} The processed CSS content.
+ */
+async function processCSS(url: string): Promise<string> {
+  // Fetch the CSS content from the specified URL
+  const response = await fetch(url);
 
   if (!response.ok) {
     throw new Error(
-      `Failed to fetch theme: ${response.status} - ${response.statusText}`,
+      `Failed to fetch theme: ${url}: ${response.status} - ${response.statusText}`,
     );
   }
 
-  return await response.text();
+  const text = await response.text();
+  const plugins: postcss.AcceptedPlugin[] = []; // Add any necessary plugins for postcss
+
+  let parsedCss = "";
+
+  // Process the CSS content with PostCSS
+  const options: ProcessOptions = {
+    from: undefined,
+  };
+
+  const result = await postcss(plugins).process(text, options);
+
+  // Handle CSS imports (at-rules)
+  await Promise.all(
+    result.root.nodes.map(async (node) => {
+      if (
+        // check if it's an atrule (@)
+        node.type === "atrule" &&
+        // check if it's an `import` atrule (@import)
+        // are there more rules i should handle? not sure
+        node.name === "import"
+      ) {
+        const importTarget = node.params
+          // remove used to remove single or double quotes from a string
+          .replace(/['"]/g, "")
+          // we have to remove the `url(<url>)` around the actual url
+          .replace(
+            /url\((.*?)\)/g,
+            "$1",
+          );
+
+        parsedCss += await processCSS(importTarget);
+      }
+    }),
+  );
+
+  parsedCss += result.css;
+
+  return parsedCss;
 }
 
 /**
@@ -41,10 +90,7 @@ export async function setupThemeConfig(window: Window) {
   try {
     const style = await fetchTheme();
     log("Retrieved theme");
-    applyTheme(
-      window,
-      style,
-    );
+    applyTheme(window, style);
   } catch (error: any) {
     log(`Error fetching theme: ${error.message}`, LogLevel.ERROR);
   }
